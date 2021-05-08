475f4f1b2a19b5848565e0c9b28d67f9dec755b0
Swayam-555 <radheshyam.panigrahi@gmail.com>
2021-05-08 13:18:34 +0530

    day to night

--- sketch.js
+++ sketch.js
@@ -8,8 +8,11 @@ var backgroundImg;
 
 var bg ;
 
+  var time = 4;
+
 function preload() {
     // create getBackgroundImg( ) here
+    getBackgroundImg()
 }
 
 function setup(){
@@ -22,27 +25,58 @@ function setup(){
 function draw(){
 
     // add condition to check if any background image is there to add
-
+    if(backgroundImg){
+        background(backgroundImg);
+    }
 
     Engine.update(engine);
 
     // write code to display time in correct format here
-
+    fill("green")
+    textSize(30);
+    text("TIME:"+time,20,50)
 
 }
 
 async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
 
-    // write code to fetch time from API
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=04 && hour<=06){
+        bg = "sunrise1.png";
+    }
+    else if(hour>=04 && hour<=06){
+        bg = "sunrise1.png";
+    } else if(hour>=06 && hour<=08){
+        bg = "sunrise2.png";
+    } else if(hour>=08 && hour<=10){
+        bg = "sunrise3.png";
+    } else if(hour>=10 && hour<=12){
+        bg = "sunrise4.png";
+    } else if(hour>=12 && hour<=14){
+        bg = "sunrise5.png";
+    } else if(hour>=14 && hour<=15){
+        bg = "sunrise6.png";
+    } else if(hour>=15 && hour<=17){
+        bg = "sunset7.png";
+    } else if(hour>=17 && hour<=18){
+        bg = "sunset8.png";
+    } else if(hour>=18 && hour<=19){
+        bg = "sunset9.png";
+    } else if(hour>=19 && hour<=21){
+        bg = "sunset10.png";
+    } else if(hour>=21 && hour<=22){
+        bg = "sunset11.png";
+    } else {
+        bg = "sunset12.png";
+    }
 
-    //change the data in JSON format
+    
 
-    // write code slice the datetime
-
-
-    // add conditions to change the background images from sunrise to sunset
-
-
-    //load the image in backgroundImg variable here
-
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
 }
+
